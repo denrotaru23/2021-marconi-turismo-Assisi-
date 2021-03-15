605c7a8db0a2f353765adf1e73b7da62464286f6
robertamor <roberta.moracchioli@marconicloud.it>
2021-03-15 17:18:55 +0000

    js

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-let receiveData = function(data) {
-    console.log(data);
-    $("#titolo").text(data.titolo);
+var create_dyn_card = function (jsonData) {
+  console.log(jsonData);
+
+  let points = jsonData.points;
+
+ for(points of point){
+  let movie = $("<div>").addClass("movie");
+  let menu_card = $("<div>").addClass("menu_card");
+  let menu_img = $("<img>").attr('src', point.img_url);
+  let desc= $("<p>").text(point.descr);
+
+
+  $(".flexcontainer").append(menu_card);
+  menu_card.append(menu_img);
+  menu_card.append(descr);
+  
+  ;
+
+   }
 }
 
-let init = function() {
-  $.getJSON("poi_list.json")
-  .done(receiveData)
-  .fail();
-  //$.getJSON("index.php").done(receiveData);
+
+let onFail = function (error) {
+  console.log("errore nella lettura del file json");
+  console.log(error);
+}
+let init = function () {
+  $.getJSON("data.json")
+    .done(create_dyn_card)
+    .fail(onFail);
+  //$.getJSON("index.php").done(create_dyn_card);
 }
 
 $(document).ready(init)
