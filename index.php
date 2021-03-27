080b32881b4aa10972a95136221e24552af5bafc
robertamor <roberta.moracchioli@marconicloud.it>
2021-03-27 09:46:13 +0000

    js

--- index.php
+++ index.php
@@ -3,16 +3,22 @@ $json = new stdClass();
 $json-> points= array(
   new stdClass(),
   new stdClass(),
+  new stdClass(),
   new stdClass()
   );
-$json-> points[0]-> "movie"= "video.mp4";
-$json-> points[1]-> "img_url"= "basilica.jpg";
+$json-> points[0]-> "type"= "video";
+$json-> points[0]-> "url"= "video.mp4";
+
+$json-> points[1]-> "type"= "immagine";
+$json-> points[1]-> "url"= "basilica.jpg";
 $json-> points[1]-> "descr"= " La prima pietra della Basilica di San Francesco d’Assisi venne posata il 16 luglio del 1228, a soli due giorni dalla morte di Francesco e all’indomani della sua proclamazione a Santo da parte di Papa Gregorio IX. Secondo la tradizione fu lo stesso Francesco a indicare con esattezza il luogo in cui desiderava essere sepolto: la collina inferiore della città umbra dove abitualmente venivano sepolti i senza legge, i condannati dalla giustizia. La chiesa rappresenta uno dei capisaldi della diffusione del gotico in Italia. La costruzione della basilica venne avviata nel 1228 da Papa Gregorio IX per poi essere consacrata nel 1253 da Papa Innocenzo IV.";
 
-$json-> points[2]-> "img_url"= "affreschi.jpg";
+$json-> points[2]-> "type"= "immagine";
+$json-> points[2]-> "url"= "affreschi.jpg";
 $json-> points[2]-> "descr"= "Gli affreschi che decorano la basilica di San Francesco d’Assisi rappresentano capolavori  immortali dell’arte italiana. I più celebri sono quelli che formano il ciclo pittorico “Storie di San Francesco” della navata della chiesa superiore. Divise in 28 episodi, le scene raccontano i momenti più significativi della vita del santo serafico.”. Le “Storie di San Francesco” sono caratterizzate da soluzioni formali rivoluzionarie, a cominciare dall’impaginazione delle scene che si differenzia dalle cornici geometriche dei pittori del ‘200";
 
-$json-> points[3]-> "img_url"= "basinf.jpg";
+$json-> points[3]-> "type"= "immagine";
+$json-> points[3]-> "url"= "basinf.jpg";
 $json-> points[3]-> "descr"= "La chiesa inferiore fu iniziata nel 1228 e terminata due anni dopo, quando vi fu traslato il corpo di San Francesco, deposto in un sarcofago sotto l’altare maggiore, nel punto in cui è conservato ancora oggi all’interno di una piccola cripta. Le decorazioni della chiesa inferiore comprendono i lavori degli artisti più illustri del tempo, come Giotto, Cimabue e Simone Martini. La chiesa inferiore è stata affrescata con scene nel transetto ad opera del Maestro di San Francesco e successivamente da Cimabue, che dipinse la Maestà"; 
 
 
--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-var create_dyn_card = function (jsonData) {
-  console.log(jsonData);
+var create_dyn_card = function (data) {
+  console.log(data);
 
-  let points = jsonData.points;
+let points = data.points;
 
- for(points of point){
-  let movie = $("<div>").addClass("movie");
+
+for(point of points){
+
   let menu_card = $("<div>").addClass("menu_card");
-  let menu_img = $("<img>").attr('src', point.img_url);
   let desc= $("<p>").text(point.descr);
+  let url;
+  if(point.url){
+    if(point.type=="immagine"){
+    url = $("<img>").attr('src', point.url)
+    }else if(point.type=="video"){
+     url=$("<video controls>").attr('src', point.url);
+  }
+  }
 
 
-  $(".flexcontainer").append(menu_card);
-  menu_card.append(menu_img);
-  menu_card.append(descr);
+$(".flexcontainer").append(menu_card);
   
-  ;
+  menu_card.append(url);
+  menu_card.append(desc);
 
-   }
-}
+ 
+  }
+  }
 
 
 let onFail = function (error) {
@@ -25,10 +33,9 @@ let onFail = function (error) {
   console.log(error);
 }
 let init = function () {
-  $.getJSON("data.json")
+ $.getJSON("data.json")
     .done(create_dyn_card)
-    .fail(onFail);
-  //$.getJSON("index.php").done(create_dyn_card);
+   .fail(onFail);
+//$.getJSON("data.php").done(create_dyn_card);
 }
-
 $(document).ready(init)
